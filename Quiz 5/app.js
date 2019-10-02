cd3f3340b698388e2c9117643f90a29a44a7a63e
Khizar723 <Khizar723@users.noreply.github.com>
2019-10-02 19:33:50 -0400

    Update app.js

--- Quiz 5/app.js
+++ Quiz 5/app.js
@@ -3,15 +3,32 @@
 
 const list = document.querySelectorAll('li');
 const listArray = Array.from(list);
-//console.log(listArray);    we gucci here
+console.log(listArray);   
 
 
- function keyWord(listArray){
+/*  function keyWord(listArray){
     if(listArray.textContent === 'Flexbox') {
         return listArray;
     }
 }
+ */
+const filtered = listArray.filter(function(filters){
+    if (filters.value === 'Flexbox' ) {
+      return 'Hellos' ;
+    }
+ })
+console.table(filtered)
 
 
-const listFiltered = listArray.filter(keyWord);
-// console.log(listFiltered);   not gucci; maybe function needs to target each array element rather than the entire array
+
+/* 
+function filterItems(arr, query) {
+    return arr.filter(function(el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+
+    })
+}
+ */
+        //console.log(filterItems(listArray, 'box'))
+//const listFiltered = listArray.filter('Flexbox');
+//console.log(listFiltered); 
