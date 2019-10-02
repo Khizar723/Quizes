2a5250a7551f621ba46de97d649dc92922959dc8
Khizar723 <Khizar723@users.noreply.github.com>
2019-10-01 22:05:55 -0400

    Update app.js

--- Quiz 5/app.js
+++ Quiz 5/app.js
@@ -1,110 +1,17 @@
 //const number = document.querySelector('#list')
 
 
-const hello1 = {
-    datatime : 5.17 ,
-    name: 'Flexbox Video'
-}
+const list = document.querySelectorAll('li');
+const listArray = Array.from(list);
+//console.log(listArray);    we gucci here
 
-const hello2 = {
-    datatime : 8.22 ,
-    name: 'Flexbox Video'
-}
 
-const hello3 = {
-    datatime : 3.34 ,
-    name: 'Redux Video'
-}
-
-const hello4 = {
-    datatime : 5.23 ,
-    name: 'Flexbox Video'
-}
-
-const hello5 = {
-    datatime : 7.12 ,
-    name: 'Flexbox Video'
-}
-
-const hello6 = {
-    datatime : 7.24 ,
-    name: 'Redux Video'
-}
-
-const hello7 = {
-    datatime : 6.46 ,
-    name: 'Flexbox Video'
-}
-
-const hello8 = {
-    datatime : 4.45 ,
-    name: 'Flexbox Video'
-}
-
-const hello9 = {
-    datatime : 4.40 ,
-    name: 'Flexbox Video'
-}
-
-const hello10 = {
-    datatime : 7.58 ,
-    name: 'Redux Video'
-}
-
-const hello11 = {
-    datatime : 11.51 ,
-    name: 'Flexbox Video'
-}
-
-const hello12 = {
-    datatime : 9.13 ,
-    name: 'Flexbox Video'
-}
-
-const hello13 = {
-    datatime : 5.50 ,
-    name: 'Flexbox Video'
-}
-
-const hello14 = {
-    datatime : 5.52 ,
-    name: 'Redux Video'
+ function keyWord(listArray){
+    if(listArray.textContent === 'Flexbox') {
+        return listArray;
+    }
 }
 
 
-const hello15 = {
-    datatime : 5.49 ,
-    name: 'Flexbox Video'
-}
-
-const hello16 = {
-    datatime : 8.57 ,
-    name: 'Flexbox Video'
-}
-
-const hello17 = {
-    datatime : 11.29 ,
-    name: 'Flexbox Video'
-}
-
-const hello18 = {
-    datatime : 3.07 ,
-    name: 'Redux Video'
-}
-
-const hello19 = {
-    datatime : 5.59 ,
-    name: 'Redux Video'
-}
-
-const hello20 = {
-    datatime : 3.31 ,
-    name: 'Flexbox Video'
-}
-
-
-
-let list = [hello1,hello2,hello3,hello4,hello5,hello6,hello7,hello8,hello9,hello10,hello11,hello12,hello13,hello14,hello15,hello16,hello17,hello18,hello19,hello20]
-
-console.log(list)
-
+const listFiltered = listArray.filter(keyWord);
+// console.log(listFiltered);   not gucci; maybe function needs to target each array element rather than the entire array
