8baeb669dfb43947e87c78fc5ebf71cfb22fdb4a
Khizar723 <Khizar723@users.noreply.github.com>
2019-10-03 11:46:14 -0400

    Update app.js

--- Quiz 5/app.js
+++ Quiz 5/app.js
@@ -1,9 +1,9 @@
 //const number = document.querySelector('#list')
 
 
-const list = document.querySelectorAll('li');
-const listArray = Array.from(list);
-console.log(listArray);   
+//const list = document.querySelectorAll('li');
+//const listArray = Array.from(list);
+//console.log(listArray);   
 
 
 /*  function keyWord(listArray){
@@ -12,12 +12,12 @@ console.log(listArray);
     }
 }
  */
-const filtered = listArray.filter(function(filters){
-    if (filters.value === 'Flexbox' ) {
-      return 'Hellos' ;
-    }
- })
-console.table(filtered)
+//const filtered = listArray.filter(function(filters){
+    //if (filters.value === 'Flexbox' ) {
+     // return 'Hellos' ;
+   // }
+ //})
+//console.table(filtered)
 
 
 
@@ -32,3 +32,22 @@ function filterItems(arr, query) {
         //console.log(filterItems(listArray, 'box'))
 //const listFiltered = listArray.filter('Flexbox');
 //console.log(listFiltered); 
+
+const LIs = Array.from(document.querySelectorAll('ul>li'));
+//console.log(LIs)
+
+const LIsFiltered = LIs.filter(LI => LI.textContent == "Flexbox Video");
+//console.log(LIsFiltered)
+
+const filteredChained = Array.from(document.querySelectorAll('ul>li'))
+.filter(LI => LI.textContent == "Flexbox Video")
+.map(obj =>{return obj.dataset.time;})
+.map( timeCode => {
+    const parts = timeCode.split(':');
+    //console.log(parts);
+    return (parseInt(parts[0] * 60)) + parseInt(parts[1]);
+}).reduce((acc, val) => {
+    return acc + val;
+})
+
+console.log(filteredChained);
